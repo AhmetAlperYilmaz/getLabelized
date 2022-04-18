9e52d519d8b38834dbf30b2a6b81ad79679817ab
AhmetAlperYilmaz <alperyilmaz2000@hotmail.com>
2022-04-18 22:00:11 +0300

    null expection error continues 2

--- src/main/java/com/alper/model/menu/Label.java
+++ src/main/java/com/alper/model/menu/Label.java
@@ -1,11 +1,22 @@
 package com.alper.model.menu;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Label {
     private long labelID;
     private String labelname;
+
+    public long getLabelID() {
+        return labelID;
+    }
+
+    public void setLabelID(long labelID) {
+        this.labelID = labelID;
+    }
+
+    public String getLabelname() {
+        return labelname;
+    }
+
+    public void setLabelname(String labelname) {
+        this.labelname = labelname;
+    }
 }
